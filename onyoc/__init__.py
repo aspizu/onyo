dcaeab63c139160ba9d3519b97cd33706e9762df
aspizu <aspizu@protonmail.com>
2023-09-03 23:45:08 +0530

    Type checker

--- onyoc/__init__.py
+++ onyoc/__init__.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from importlib.resources import files
 from pathlib import Path
-from typing import cast
+from typing import Union, cast
 
 from lark import Lark, Token, Tree, UnexpectedCharacters
 from lark.visitors import Visitor
@@ -16,6 +16,49 @@ parser = Lark(
 from . import term as t
 
 
+class TypeEnum:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, TypeEnum):
+            return self.name == other.name
+        return False
+
+    def __str__(self):
+        return self.name
+
+
+class Type:
+    T = Union[TypeEnum, "UNION"]
+
+    ANY = TypeEnum("any")
+    NULL = TypeEnum("null")
+    BOOL = TypeEnum("bool")
+    INT = TypeEnum("int")
+    FLOAT = TypeEnum("float")
+    STR = TypeEnum("str")
+
+    class UNION:
+        def __init__(self, *types: "Type.T"):
+            self.types: list["Type.T"] = []
+            for type in types:
+                self.add_type(type)
+
+        def add_type(self, type: "Type.T"):
+            if type in self.types:
+                return
+            if isinstance(type, Type.UNION):
+                for subtype in type.types:
+                    self.add_type(subtype)
+
+        def __eq__(self, other: object) -> bool:
+            return other in self.types
+
+
+type: Type.T = Type.UNION(Type.NULL, Type.BOOL)
+
+
 @dataclass
 class Range:
     line: int
@@ -27,7 +70,11 @@ class Range:
         if isinstance(token, Token):
             assert token.line is not None
             assert token.column is not None
-            return cls(token.line - 1, token.column - 1, len(token))
+            return cls(
+                token.line - 1,
+                token.column - 1,
+                len(token),
+            )
         else:
             return cls(
                 token.meta.line - 1,
@@ -60,7 +107,7 @@ class Error:
             if line_no == self.range.line:
                 t.w(
                     t.blue
-                    + " " * (6 + self.range.column)
+                    + " " * (7 + self.range.column)
                     + "^" * self.range.length
                     + " "
                     + self.help
@@ -132,6 +179,156 @@ class DefinitionCollector(Interpreter, ErrorStorage):
             arguments = []
         body = cast(Tree[Token], node.children[-1])
         self.functions[str(name)] = Function(name, arguments, body)
+
+
+class TypeCheckerVisitor:
+    def _call_userfunc(self, tree: Tree[Token] | Token, types: list[Type.T]) -> Type.T:
+        if isinstance(tree, Token):
+            return getattr(
+                self,
+                tree.type,
+                self.__default__,
+            )(tree)
+        else:
+            return getattr(
+                self,
+                tree.data if tree is not None else "",  # type: ignore
+                self.__default__,
+            )(tree, types)
+
+    def __default__(
+        self, tree: Tree[Token] | Token, types: list[Type.T] | None = None
+    ) -> Type.T:
+        ...
+
+    def __class_getitem__(cls, _):
+        return cls
+
+    def visit(self, tree: Tree[Token] | Token) -> Type.T:
+        "Visits the tree, starting with the leaves and finally the root (bottom-up)"
+        types: list[Type.T] = []
+        if isinstance(tree, Tree):
+            for child in tree.children:
+                types.append(self.visit(child))
+        return self._call_userfunc(tree, types)
+
+
+Node = Tree[Token] | Token
+
+
+class TypeCheck(TypeCheckerVisitor, ErrorStorage):
+    def __init__(
+        self,
+        source: str,
+        file: Path,
+        defcol: DefinitionCollector,
+        node: Tree[Token],
+    ):
+        ErrorStorage.__init__(self, source, file)
+        self.defcol = defcol
+        self.visit(node)
+
+    def NULL(self, token: Token):
+        return Type.NULL
+
+    def INT(self, token: Token):
+        return Type.INT
+
+    def FLOAT(self, token: Token):
+        return Type.FLOAT
+
+    def STRING(self, token: Token):
+        return Type.STR
+
+    def expr(self, node: Tree[Token], types: list[Type.T]):
+        return types[0]
+
+    def add(self, node: Tree[Token], types: list[Type.T]):
+        def aux(right: Node, left_type: Type.T, right_type: Type.T):
+            if left_type == Type.INT:
+                if right_type in (Type.INT, Type.FLOAT):
+                    return right_type
+                self.add_error(
+                    Error(
+                        f"expected int|float but got {right_type}.",
+                        Range.new(right),
+                        help="Try using float().",
+                    )
+                )
+                return Type.INT
+            if left_type == Type.FLOAT:
+                if right_type not in (Type.INT, Type.FLOAT):
+                    self.add_error(
+                        Error(
+                            f"expected int|float but got {right_type}.",
+                            Range.new(right),
+                            help="Try using float().",
+                        )
+                    )
+                return Type.FLOAT
+            if left_type == Type.STR:
+                if right_type != Type.STR:
+                    self.add_error(
+                        Error(
+                            f"expected str but got {right_type}.",
+                            Range.new(right),
+                            help="Try using str().",
+                        )
+                    )
+                return Type.STR
+
+        if ok := aux(node.children[1], types[0], types[1]):
+            return ok
+        if ok := aux(node.children[0], types[1], types[0]):
+            return ok
+        self.add_error(
+            Error(
+                f"expected int|float|str but got {types[0]}",
+                Range.new(node.children[0]),
+            )
+        )
+        return Type.FLOAT
+
+    def __binary(self, node: Tree[Token], types: list[Type.T]):
+        def aux(right: Node, left_type: Type.T, right_type: Type.T):
+            if left_type == Type.INT:
+                if right_type in (Type.INT, Type.FLOAT):
+                    return right_type
+                self.add_error(
+                    Error(
+                        f"expected int|float but got {right_type}.",
+                        Range.new(right),
+                        help="Try using float().",
+                    )
+                )
+                return Type.INT
+            if left_type == Type.FLOAT:
+                if right_type not in (Type.INT, Type.FLOAT):
+                    self.add_error(
+                        Error(
+                            f"expected int|float but got {right_type}.",
+                            Range.new(right),
+                            help="Try using float().",
+                        )
+                    )
+                return Type.FLOAT
+
+        if ok := aux(node.children[1], types[0], types[1]):
+            return ok
+        if ok := aux(node.children[0], types[1], types[0]):
+            return ok
+        self.add_error(
+            Error(
+                f"expected int|float but got {types[0]}",
+                Range.new(node.children[0]),
+            )
+        )
+        return Type.FLOAT
+
+    sub = __binary
+    mul = __binary
+    div = __binary
+    mod = __binary
 
 
 class Linter(Visitor[Token], ErrorStorage):
@@ -398,6 +595,8 @@ def compile(input: Path, output: Path):
     errors.extend(defcol.errors)
     linter = Linter(source, input, defcol, root)
     errors.extend(linter.errors)
+    typechecker = TypeCheck(source, input, defcol, root)
+    errors.extend(typechecker.errors)
     compiler = Compiler(source, input, output, root)
     errors.extend(compiler.errors)
     return errors
